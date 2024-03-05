9202ac55a516650c7fd17efb9011f89ecd94f492
megyang <114083293+megyang@users.noreply.github.com>
2024-03-05 14:07:41 -0500

    next app

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import SocialSecurityCalculator from "@/app/calculator/SocialSecurityCalculator";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
