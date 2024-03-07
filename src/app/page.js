decc0464dd2d1676d341b02c1d747371936b68bf
megyang <114083293+megyang@users.noreply.github.com>
2024-03-07 18:02:27 -0500

    load and delete feature

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import SupabaseProvider from "@/app/providers/SupabaseProvider";
 import UserProvider from "@/app/providers/UserProvider";
 import Header from "@/app/Header";
 import ModalProviders from "@/app/providers/ModalProviders";
+import {MyUserContextProvider} from "@/app/hooks/useUser";
 
 //will not be recached and will always be up to date
 export const revalidate = 0;
@@ -11,13 +12,15 @@ export default function Page() {
     return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-          <SupabaseProvider>
-              <UserProvider>
-                  <ModalProviders />
-                    <Header />
-                    <SocialSecurityCalculator />
-                </UserProvider>
-          </SupabaseProvider>
+          <MyUserContextProvider>
+              <SupabaseProvider>
+                  <UserProvider>
+                      <ModalProviders />
+                        <Header />
+                        <SocialSecurityCalculator />
+                    </UserProvider>
+              </SupabaseProvider>
+          </MyUserContextProvider>
       </div>
     </main>
   );
