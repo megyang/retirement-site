64c952fec64beb7a9aecb7dcd562a5701e1d39e4
megyang <114083293+megyang@users.noreply.github.com>
2024-08-15 16:58:38 -0400

    vercel

--- src/app/modal/AuthModal.js
+++ src/app/modal/AuthModal.js
@@ -1,8 +1,7 @@
 "use client";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from "@/app/modal/Modal";
 import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import useAuthModal from "@/app/hooks/useAuthModal";
@@ -11,6 +10,8 @@ const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const { onClose, isOpen, view, setView } = useAuthModal();
     const { session } = useSessionContext();
+    const [email, setEmail] = useState('');
+
     useEffect(() => {
         if (session) {
             onClose();
@@ -22,6 +23,8 @@ const AuthModal = () => {
             return 'Log In';
         } else if (view === 'sign_up') {
             return 'Sign Up';
+        } else if (view === 'forgot_password') {
+            return 'Reset Password';
         } else {
             return 'Welcome';
         }
@@ -32,21 +35,21 @@ const AuthModal = () => {
             return 'Please log in to continue';
         } else if (view === 'sign_up') {
             return 'Create an account to plan for retirement';
+        } else if (view === 'forgot_password') {
+            return 'Enter your email to reset your password';
         } else {
             return 'Please sign in to continue';
         }
     };
 
-    const localization = {
-        variables: {
-            sign_up: {
-                email_label: 'Email',
-                password_label: 'Password',
-                password_input_placeholder: 'Create a password'
-            },
-            sign_in: {
-                email_label: 'Email',
-                password_label: 'Password'
+    const handlePasswordReset = async () => {
+        if (email) {
+            const { data, error } = await supabaseClient.auth.resetPasswordForEmail(email);
+            if (error) {
+                console.error('Error sending reset password email:', error);
+            } else {
+                alert('Password reset email sent!');
+                setView('sign_in');
             }
         }
     };
@@ -57,33 +60,86 @@ const AuthModal = () => {
             description={getDescription()}
             isOpen={isOpen}
             onChange={onClose}>
-            <Auth
-                providers={[]}
-                magicLink={false}
-                showLinks={false}
-                supabaseClient={supabaseClient}
-                view={view}
-                appearance={{
-                    theme: ThemeSupa,
-                    variables: {
-                        default: {
-                            colors: {
-                                brand: '#404040',
-                                brandAccent: '#c0e0f8'
+            {view === 'forgot_password' ? (
+                <div>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email"
+                        style={{
+                            width: '100%',
+                            padding: '10px',
+                            marginTop: '20px',
+                            fontSize: '1rem',
+                            borderRadius: '4px',
+                            border: '1px solid #ccc',
+                        }}
+                    />
+                    <button
+                        onClick={handlePasswordReset}
+                        style={{
+                            width: '100%',
+                            padding: '10px',
+                            marginTop: '20px',
+                            fontSize: '1rem',
+                            borderRadius: '4px',
+                            backgroundColor: '#404040',
+                            color: 'white',
+                            cursor: 'pointer',
+                            border: 'none'
+                        }}
+                    >
+                        Send Reset Link
+                    </button>
+                </div>
+            ) : (
+                <Auth
+                    providers={[]}
+                    magicLink={false}
+                    showLinks={false}
+                    supabaseClient={supabaseClient}
+                    view={view}
+                    appearance={{
+                        theme: ThemeSupa,
+                        variables: {
+                            default: {
+                                colors: {
+                                    brand: '#404040',
+                                    brandAccent: '#c0e0f8'
+                                }
                             }
                         }
-                    }
-                }}
-                localization={view === 'sign_up' || view === 'sign_in' ? localization : {}}
-            />
+                    }}
+                />
+            )}
             <div style={{ marginTop: '20px', textAlign: 'center', fontSize: '0.875rem', color: 'gray' }}>
                 {view === 'sign_in' ? (
+                    <>
+                        <p>
+                            <button
+                                onClick={() => setView('sign_up')}
+                                style={{ color: 'gray', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
+                            >
+                                Do not have an account? Sign up
+                            </button>
+                        </p>
+                        <p>
+                            <button
+                                onClick={() => setView('forgot_password')}
+                                style={{ color: 'gray', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
+                            >
+                                Forgot your password?
+                            </button>
+                        </p>
+                    </>
+                ) : view === 'sign_up' ? (
                     <p>
                         <button
-                            onClick={() => setView('sign_up')}
+                            onClick={() => setView('sign_in')}
                             style={{ color: 'gray', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
                         >
-                            Do not have an account? Sign up
+                            Already have an account? Sign in
                         </button>
                     </p>
                 ) : (
@@ -92,7 +148,7 @@ const AuthModal = () => {
                             onClick={() => setView('sign_in')}
                             style={{ color: 'gray', textDecoration: 'underline', background: 'none', border: 'none', cursor: 'pointer' }}
                         >
-                            Already have an account? Sign in
+                            Back to Log In
                         </button>
                     </p>
                 )}
